791a2a4f57f2d068faf766ea2c73ef5ad14c6e81
MSturges <Sturges3232@gmail.com>
2016-06-29 13:10:30 -0600

    cr of crud working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,16 @@ class UsersController < ApplicationController
     render :json => {id: user.id, userName: user.userName}
   end
 
+  def create
+    user = User.new(userName: params[:userName], password_digest: params[:password_digest], lat: params[:lat], long: params[:long])
+    # if the user is saved successfully than respond with json data and status code 201
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors}, status: 422
+    end
+  end
+
+
+
 end
