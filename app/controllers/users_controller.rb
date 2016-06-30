a30182287d37fc15efa713450093d7f01a6b078b
MSturges <Sturges3232@gmail.com>
2016-06-30 14:09:56 -0600

    added cores to application_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,15 @@ def index
   render json: User.all
 end
 
+
+
+
 def show
   user = User.find_by(id: params[:id])
   render :json => {id: user.id, userName: user.userName}
 end
+
+
 
 def create
   user = User.new(user_params)
@@ -17,6 +22,10 @@ def create
     render json: { errors: user.errors}, status: 422
   end
 end
+
+
+
+
 
 def update
   user = User.find_by(id: params[:id])
@@ -32,7 +41,7 @@ def destroy
   user.destroy
 end
 
-# $http.post('/users', {user: userObj})
+# $http.post('/users', {user: userObj} )
 # $http.patch('/users', {user: latLng})
 
 private def user_params
