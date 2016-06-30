3696314c277cfb0301bb5868ce4fee28f0589eeb
MSturges <Sturges3232@gmail.com>
2016-06-30 13:14:25 -0600

    reversed back to sign up with username, pass, lat, long

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,42 @@
 class UsersController < ApplicationController
 
-  def index
-    render json: User.all
-  end
+def index
+  render json: User.all
+end
 
-  def show
-    user = User.find_by(id: params[:id])
-    render :json => {id: user.id, userName: user.userName}
-  end
+def show
+  user = User.find_by(id: params[:id])
+  render :json => {id: user.id, userName: user.userName}
+end
 
-  def create
-    user = User.new(user_params)
-    # if the user is saved successfully than respond with json data and status code 201
-    if user.save
-      render json: user, status: 201
-    else
-      render json: { errors: user.errors}, status: 422
-    end
+def create
+  user = User.new(user_params)
+  if user.save
+    render json: user, status: 201
+  else
+    render json: { errors: user.errors}, status: 422
   end
+end
 
-  def update
-    user = User.find_by(id: params[:id])
-    if user.update(params.require(:user).permit(:lat, :long))
-      render json: user, status: 201
-    else
-      render json: { errors: user.errors}, status: 422
-    end
+def update
+  user = User.find_by(id: params[:id])
+  if user.update(params.require(:user).permit(:lat, :long))
+    render json: user, status: 201
+  else
+    render json: { errors: user.errors}, status: 422
   end
+end
 
-  def destroy
-    user = User.find_by(id: params[:id])
-    user.destroy
-  end
+def destroy
+  user = User.find_by(id: params[:id])
+  user.destroy
+end
 
-  # $http.post('/users', {user: userObj})
-  # $http.patch('/users', {user: latLng})
-  private def user_params
-    params.require(:user).permit(:userName, :password)
-  end
+# $http.post('/users', {user: userObj})
+# $http.patch('/users', {user: latLng})
+
+private def user_params
+  params.require(:user).permit(:userName, :password, :lat, :long)
+end
 
 end
