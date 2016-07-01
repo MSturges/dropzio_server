784ff798cfb99edf1f4accef68534c442d9e2216
MSturges <Sturges3232@gmail.com>
2016-07-01 11:11:23 -0600

    rails...i hate u

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,17 +1,12 @@
 class LoginsController < ApplicationController
 
-  # if you get a token error try uncommenting the line below
-  # skip_before_filter  :verify_authenticity_token
-
   def login
     user = User.find_by(userName: params[:user][:userName])
-    if user.authenticate(params[:user][:password])
-        render json: user, status: 201
+
+    if !user || user.authenticate(params[:user][:password])
+      render json: user, status: 201
     else
       render status: 200, :json => {error: "Name or password is incorrect"}
     end
   end
-
-
-
 end
