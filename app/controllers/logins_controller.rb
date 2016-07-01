31dc6a1d51e96c049e8415df88849ead63d80ad7
MSturges <Sturges3232@gmail.com>
2016-07-01 10:39:27 -0600

    basic login functionality will make better

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,33 +1,17 @@
 class LoginsController < ApplicationController
 
-  include BCrypt
-  skip_before_filter  :verify_authenticity_token
+  # if you get a token error try uncommenting the line below
+  # skip_before_filter  :verify_authenticity_token
 
-  def signin
-
+  def login
     user = User.find_by(userName: params[:user][:userName])
-    @password ||= Password.new(password_hash)
-    # puts user.password == params[:user][:password]
-    puts @password
-    puts params[:user][:password]
-
-    if @password == params[:user][:password]
-      puts user
-      render :json => {id: user.id, name: user.userName}
+    if user.authenticate(params[:user][:password])
+      render :json => {id: user.id, name: user.userName, password: user.password_digest}
     else
-      render status: 400, :json => {error: "Name or password is incorrect"}
+      render status: 200, :json => {error: "Name or password is incorrect"}
     end
   end
 
+  
+
 end
-
-
-# def signin
-#   user = User.find_by(name: params[:name])
-#   if user.password == params[:password]
-#     jwt = JsonWebToken.sign({id: user.id}, key: ENV['SECRET'])
-#     render :json => {id: user.id, name: user.name, jwt: jwt}
-#   else
-#     render status: 400, :json => {error: "Name or password is incorrect"}
-#   end
-# end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
 
-  post '/login', to: "logins#signin"
+  post '/login', to: "logins#login"
 
 end
