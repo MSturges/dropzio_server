88e0e63c5015509c23f30c174f6e2a53a41dd350
MSturges <Sturges3232@gmail.com>
2016-07-04 15:48:18 -0600

    added match any options in our routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  match '*any' => 'application#options', :via [:options]
+
   resources :users
   resources :posts
   resources :uploads
 
   post '/login', to: "logins#login"
-  
+
   get '/postlist', to: "posts#getposts"
 
   post '/changescore', to: "posts#changescore"
