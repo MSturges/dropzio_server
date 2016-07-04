f36fe3e37f5ea4567381baefcd584c646d49327c
MSturges <Sturges3232@gmail.com>
2016-07-04 14:34:00 -0600

    using :flat equation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   acts_as_mappable :default_units => :miles,
-  :default_formula => :sphere,
+  :default_formula => :flat,
   :distance_field_name => :distance,
   :lat_column_name => :lat,
   :lng_column_name => :long
