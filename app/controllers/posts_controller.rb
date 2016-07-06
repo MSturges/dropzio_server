a74f56192ca6633cb6e03298b26ab89773d89b83
MSturges <Sturges3232@gmail.com>
2016-07-05 21:49:31 -0600

    ...

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
 
     posistion = params.require(:compass).permit(:lat, :long)
 
-    posts = Post.in_range(:all, :origin => [params[:compass][:lat], params[:compass][:long]], :within = 10.0 )
+    posts = Post.in_range(:all, :origin => [params[:lat], params[:long]], :within = 10.0 )
 
 
     render json: {posts: posts}
