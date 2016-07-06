cfd1468c8a058366f233b619bb49cb1485b6ba58
MSturges <Sturges3232@gmail.com>
2016-07-05 21:06:55 -0600

    try this

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,8 +29,7 @@ class PostsController < ApplicationController
 
   def compass
 
-    posistion = request.request_parameters.slice(:lat, :long)
-
+    posistion = params.require(:post).permit(:lat, :long)
     posts = Post.in_range(:all, :origin => [posistion.lat, posistion.long], :beyond = 0.015, :within=> 0.5).order('id')
 
     if posts
